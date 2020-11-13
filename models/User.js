b9e452c679b460f36ca48b56306ac5ddc1ce84dc
Moumenh <63558498+Moumenh@users.noreply.github.com>
2020-11-13 15:42:54 +0200

    Update User.js
    making a better indentation and remove note property

--- models/User.js
+++ models/User.js
@@ -2,20 +2,41 @@ const mongoose = require('mongoose')
 
 
 //Scheema
-
 const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String
+    },
+    mobile: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
 
-    name :{type:String, required:true},
-    email :{type:String, required:true, unique: true},
-    password :{type:String, required:true},
-    token :{type:String, required:true},
-    mobile :{type:Number, required:true, unique: true},
-    address :{type:String, required:true},
-    note :{type:String, required:true}
-
-}, {timestamps: true})
+}, { timestamps: true })
 
 
+//Create the model according to Scheema
 const User = mongoose.model('User', userSchema);
+
+
+//exporting the model to use mongoose functions 
+module.exports = User
 
 
