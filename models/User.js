e2305f183eb1ae51c03770b6d3b89160b3d97626
Moumenh <mohanbali94@gmail.com>
2020-11-21 20:24:13 +0200

    email validation

--- models/User.js
+++ models/User.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose')
 
 
 //Scheema
+const validateEmail = (email) => {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+}
+
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -9,12 +15,17 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        required: true,
-        unique: true
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength : 6
     },
     token: {
         type: String
