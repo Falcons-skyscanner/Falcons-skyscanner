b989c3b8937bece434dd9a7239bda29570fa93ec
Moumenh <mohanbali94@gmail.com>
2020-11-21 11:22:50 +0200

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path')
 
 // express initializations
 const app = express()
